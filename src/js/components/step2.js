42e679ae8ce445d035fa536f655e1b2283e3565d
jcherqui <joachim.cherqui@viacesi.fr>
2016-10-30 11:31:13 +0100

    Add ininital steps

---
+++ src/js/components/step2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const step2 = () => {
+    return (<div>
+        <div className="colors">
+            <div className="color" />
+            <div className="color" />
+            <div className="color" />
+            <div className="color" />
+        </div>
+        <div className="colors">
+            <div className="color" />
+            <div className="color" />
+            <div className="color" />
+            <div className="color" />
+        </div>
+    </div>);
+};
+
+export default step2;

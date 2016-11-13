6b7a2f629a29927ebc82f0712df4723a451554e0
jcherqui <joachim.cherqui@viacesi.fr>
2016-11-13 23:09:13 +0100

    Handle localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import App from './components/App';
+import { loadState, saveState } from './localStorage';
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistedState = loadState();
+const store = createStore(
+    reducers,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 store.subscribe(() => {
+    saveState({ book: store.getState().book });
     console.log('store changed', store.getState());
 });
 

db4ea0d532acb244263840cf6e167045d71e69ec
jcherqui <joachim.cherqui@viacesi.fr>
2016-11-13 23:33:05 +0100

    Add format step

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import BookBinding from '../containers/BookBinding';
 import Color from '../containers/Color';
 import Pages from '../containers/Pages';
+import Format from '../containers/Format';
 import Gilding from '../containers/Gilding';
 
 const App = () => {
     return (
         <div>
-            <div>Test module</div>
+            <h3>Test module</h3>
             <BookBinding />
             <Color />
+            <Format />
             <Pages />
             <Gilding />
+            <button>Payment</button>
         </div>
     );
 };

ddfa4595055affd8f5ce0790062a3b676a707b07
ston3o <5039608+ston3o@users.noreply.github.com>
2016-11-19 15:05:44 +0100

    Add next and previous buttons

--- src/js/containers/Steps.js
+++ src/js/containers/Steps.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import Steps from 'react-steps';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Steps from 'react-steps';
+import { nextStep, previousStep } from '../actions/index';
+import BookBinding from './BookBinding';
+import Color from './Color';
+import Pages from './Pages';
+import Format from './Format';
+import Gilding from './Gilding';
 
 class StepsClass extends Component {
     render() {
-        return <div><Steps items={this.props.steps} type={'point'} /></div>;
+        console.log(this.props.currentStep);
+        return (<div>
+            <Steps items={this.props.steps} type={'point'} />
+            { this.props.currentStep === 1 ? <BookBinding /> : null }
+            { this.props.currentStep === 2 ? <Color /> : null }
+            { this.props.currentStep === 3 ? <Format /> : null }
+            { this.props.currentStep === 4 ? <Pages /> : null }
+            { this.props.currentStep === 5 ? <Gilding /> : null }
+            <button onClick={() => this.props.previousStep()}>Previous</button>
+            <button onClick={() => this.props.nextStep()}>Next</button>
+        </div>);
     }
 }
 
 function mapStateToProps(state) {
     return {
-        steps: state.steps,
+        steps: state.steps.steps,
+        currentStep: state.steps.currentStep,
     };
 }
 
-export default connect(mapStateToProps)(StepsClass);
+function matchDispatchToProps(dispatch) {
+    return bindActionCreators({ nextStep, previousStep }, dispatch);
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(StepsClass);

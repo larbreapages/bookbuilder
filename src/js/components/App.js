b306bbb99de9d1311706947d91fb289560fbdc26
ston3o <5039608+ston3o@users.noreply.github.com>
2016-11-27 00:28:10 +0100

    Add favicon

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Steps from '../containers/Steps';
 import Footer from '../containers/Footer';
-// import StripeCheckout from '../containers/StripeCheckout';
 
 const App = () => {
     return (<div>

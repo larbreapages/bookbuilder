fbc5e4cc0cb3ead0238252cc1d8f43268af1b007
ston3o <5039608+ston3o@users.noreply.github.com>
2017-02-25 20:44:21 +0100

    Update template

--- src/js/sendMail.js
+++ src/js/sendMail.js
@@ -13,18 +13,18 @@ const transporter = nodemailer.createTransport({
 
 const htmlOutput = mjml2html(`<mjml>
     <mj-body>
-        <mj-container>
-            <mj-section padding-bottom="0">
+        <mj-container background-color="#F1F1F1">
+            <mj-section locked="true" padding-bottom="20"></mj-section>
+            <mj-section background-color="#ffffff">
                 <mj-column>
-                    <mj-image href="https://larbreapages.fr" src="https://framapic.org/OT3L63EFS7re/g2IYJlOxZppA.png" align="center" width="360">
+                    <mj-image href="https://larbreapages.fr" src="https://framapic.org/OT3L63EFS7re/g2IYJlOxZppA.png" align="center" width="360" padding-top="0">
                     </mj-image>
+                    <mj-text width="40%" align="center" font-size="13" padding-left="25" padding-right="25" padding-bottom="0" padding-top="0">
+                        <p>Merci d'avoir choisi <b>L'Arbre à Pages</b> pour votre achat ! Votre commande a été transmise et sera bientôt traitée. Une facture vous sera envoyée sous 24h.</p>
+                    </mj-text>
                 </mj-column>
             </mj-section>
-            <mj-section padding-top="0">
-                <mj-text width="40%" align="center" font-size="13" padding-left="25" padding-right="25" padding-bottom="0" padding-top="0">
-                    <p>Merci d'avoir choisi <b>L'Arbre À Pages</b> pour votre achat ! Votre commande a été transmise et sera bientôt traitée. Une facture vous sera envoyée prochainement.</p>
-                </mj-text>
-            </mj-section>
+            <mj-section locked="true" padding-bottom="20"></mj-section>
         </mj-container>
     </mj-body>
 </mjml>`).html;

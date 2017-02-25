718d1b7cfa330cc26af4f4f18cb7fe7eebace82f
ston3o <5039608+ston3o@users.noreply.github.com>
2017-02-25 20:09:11 +0100

    Add mjml email template

--- src/js/sendMail.js
+++ src/js/sendMail.js
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import { mjml2html } from 'mjml';
 
 const transporter = nodemailer.createTransport({
     port: 587,
@@ -10,13 +11,30 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+const htmlOutput = mjml2html(`<mjml>
+    <mj-body>
+        <mj-container>
+            <mj-section padding-bottom="0">
+                <mj-column>
+                    <mj-image href="https://larbreapages.fr" src="https://framapic.org/OT3L63EFS7re/g2IYJlOxZppA.png" align="center" width="360">
+                    </mj-image>
+                </mj-column>
+            </mj-section>
+            <mj-section padding-top="0">
+                <mj-text width="40%" align="center" font-size="13" padding-left="25" padding-right="25" padding-bottom="0" padding-top="0">
+                    <p>Merci d'avoir choisi <b>L'Arbre À Pages</b> pour votre achat ! Votre commande a été transmise et sera bientôt traitée. Une facture vous sera envoyée prochainement.</p>
+                </mj-text>
+            </mj-section>
+        </mj-container>
+    </mj-body>
+</mjml>`).html;
+
 const mailOptions = {
     from: '"L\'Arbre à Pages" <contact@larbreapages.fr>',
-    to: 'contact@labreapages.fr',
     subject: 'Confirmation de commande | L\'arbre à Pages',
-    html: 'Merci d\'avoir choisi L\'Arbre À Pages pour votre achat ! Votre commande a été transmise et sera bientôt traitée. Une facture vous sera envoyée prochainement.',
+    html: htmlOutput,
 };
 
-const sendMail = ({ mail, subject, body }) => transporter.sendMail({ ...mailOptions, to: mail, subject, html: body });
+const sendMail = (email) => transporter.sendMail({ ...mailOptions, to: email });
 
 export default sendMail;

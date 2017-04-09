e7d47193f5fecae981473298953fe48edc3566f7
ston3o <5039608+ston3o@users.noreply.github.com>
2017-04-09 23:05:15 +0200

    Add unit tests

--- test/e2e/hook.js
+++ test/e2e/hook.js
@@ -13,7 +13,7 @@ const options = {
 
 before(() => {
     const server = express();
-    server.use('/', express.static(path.join(__dirname, '../dist')));
+    server.use('/', express.static(path.join(__dirname, '../../dist')));
     listeningServer = server.listen(options.port);
 
     const client = webdriverio.remote({
